88dafded68ec4343ef5452442923d4ddeba3d9f0
OmarMuAr3099 <77741710+OmarMuAr3099@users.noreply.github.com>
2021-01-20 17:49:33 +0100

    Add files via upload

---
+++ ScrCamara.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrCamara : MonoBehaviour
+{
+    [SerializeField]
+    Transform Erin;
+
+   
+
+    public Vector2 minCamPos, maxCamPos;
+    public float offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(
+            Mathf.Clamp(Erin.position.x,minCamPos.x,maxCamPos.x),
+            Mathf.Clamp(Erin.position.y+offset,minCamPos.y,maxCamPos.y), 
+            transform.position.z);
+        
+       
+    }
+}
